1c17bb1a18ca9002ee782257e7c0c58e8ec58cca
kareemeldahshoury <eldah001@umn.edu>
2025-01-29 23:44:54 -0600

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<div class="h-screen flex flex-col items-center justify-center">
+  <h1 class="text-xl">Kareem Eldahshoury</h1>
+  <p class="md:w-1/2 mt-5">
+    A hardworking, determined, learner with experience in software engineering, machine learning, data engineering, and secure coding. 
+    I am excited to apply the skills I have obtained during my education along with my experiences to make a difference in the tech industry.
+  </p>
+  <div class="mt-5">
+    <a href="/experiences" class="mr-10 font-semibold text-white bg-blue-500 rounded-lg p-2.5">Experiences</a>
+    <button class="m-5 font-semibold text-white bg-blue-500 rounded-lg p-2">Projects</button>
+    <button class="ml-10 font-semibold text-white bg-blue-500 rounded-lg p-2">Full Resume</button>
+  </div>
+</div>
